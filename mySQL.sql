5a2adf3b645685a96e99f79c9e467bf2579f8eff
Ahnaf Anan <aanana0171@gmail.com>
2023-11-08 22:53:23 +0600

    sql database

---
+++ mySQL.sql
@@ -0,0 +1,17 @@
+-- 1 --
+SELECT customers.id, customers.name, COUNT(orders.id) AS total_orders
+FROM customers
+LEFT JOIN orders ON customers.id =orders.customer_id
+ORDER BY total_orders DESC
+
+
+-- 2 -- 
+SELECT order_items.id, products.name, order_items.quantity, (order_items.quantity * order_items.unit_price) AS total_amount
+FROM
+order_items
+INNER JOIN
+products ON order_items.product_id = products.id
+ORDER BY
+order_items.id ASC
+
+-- 3 -- 
